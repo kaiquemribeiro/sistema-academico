c3c140cd23901d05fae2c6c768d38af321dbccf0
Kaique Ribeiro <kaiqueribeiro@alunos.utfpr.edu.br>
2022-05-08 16:12:00 -0300

    add akuno

--- src/js/valida.js
+++ src/js/valida.js
@@ -1,10 +1,14 @@
+document.getElementById('btn').addEventListener("click", function(event){
+    event.preventDefault();
+})
+
 const validaUsuario = () => {
     const usuario = document.getElementById('usuario').value;
     if(usuario.includes('1', 1)){
         alert('Usuario valido admin');
         return 1;
     } else if(usuario.includes('2', 1)){
-        alert('Usuario valido docente');
+        window.location.href = './aluno.html';
         return 2;
     } else {
         alert('Usuario invalido');
