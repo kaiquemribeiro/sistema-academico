5e5667ee44877e2de874b9c13daab880e52e7ae0
Kaique Ribeiro <kaiqueribeiro@alunos.utfpr.edu.br>
2022-05-08 16:14:46 -0300

    add aluno

--- src/js/valida.js
+++ src/js/valida.js
@@ -1,16 +1,14 @@
-document.getElementById('btn').addEventListener("click", function(event){
-    event.preventDefault();
-})
+document.getElementById("btn").addEventListener("click", function (event) {
+  event.preventDefault();
+});
 
 const validaUsuario = () => {
-    const usuario = document.getElementById('usuario').value;
-    if(usuario.includes('1', 1)){
-        alert('Usuario valido admin');
-        return 1;
-    } else if(usuario.includes('2', 1)){
-        window.location.href = './aluno.html';
-        return 2;
-    } else {
-        alert('Usuario invalido');
-    }
-}
+  const usuario = document.getElementById("usuario").value;
+  if (usuario.includes("1", 1)) {
+    alert("Usuario valido admin");
+  } else if (usuario.includes("2", 1)) {
+    window.location.href = "./aluno.html";
+  } else {
+    alert("Usuario invalido");
+  }
+};
