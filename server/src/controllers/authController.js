9650a09415fd4dd9b70d16f5d0971e65062a2e46
Gustavo Monteiro <ggpm20@hotmail.com.br>
2022-05-17 16:49:25 -0300

    email já cadastrado!

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -2,12 +2,12 @@ const express = require('express');
 const Student = require('../models/student');
 const router = express.Router();
 
-
 router.post('/register', async (req, res) => {
   const { email } = req.body;
 
   try {
-    if (await Student.findOne(email));
+    if (await Student.findOne({ email }))
+      return res.status(400).send({ error: 'email já cadastrado!' });
 
     const student = await Student.create(req.body);
 
